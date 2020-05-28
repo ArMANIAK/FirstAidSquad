94f51e258258cbd59ead19ae0c895f140102e971
ArMANIAK <reklama.alatyr@gmail.com>
2020-05-28 17:23:56 +0300

    dismissModal correction

--- public/scripts/solo.js
+++ public/scripts/solo.js
@@ -16,7 +16,7 @@ window.addEventListener('load', function () {
     function showModal() {
         document.querySelector('.modal_confirmation').style.display = 'block';
         document.body.style.overflow = 'hidden';
-        document.querySelector('span.dismiss').onclick = dismissModal;
+        document.querySelector('span.dismiss').onclick = turnAutoplayOff;
         document.querySelector('.turn_on').onclick = turnAutoplayOn;
         document.querySelector('.turn_off').onclick = turnAutoplayOff;
     }
