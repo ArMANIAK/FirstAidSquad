4aadbaa42f132f22205efb4f0151ef5237815b63
ArMANIAK <reklama.alatyr@gmail.com>
2020-06-02 16:29:36 +0300

    navigation menu corrections: initial commit

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -4,7 +4,14 @@
     <ul>
         <li><a href="http://redcross.org.ua" target="_blank">Товариство Червоного Хреста України</a></li>
         <li><a href="https://forms.office.com/Pages/ResponsePage.aspx?id=3mXHzJAJ1EmoDylOKKH5IzVmTTZIlmtCvrHFDQ5dqWBUNVBOTzNGSlVJQVFQTU1OWFdFQVZQUjhQUy4u" target="_blank">Зареєструватися на тренінг</a></li>
-        <li><a id="startGame" href="<?=base_url('index.php/quiz/solomode')?>">Почати гру</a></li>
+        <li><a id="startGame" href="<?=base_url('index.php/quiz/solomode')?>">Перевірити знання</a></li>
+
+        <?php if (isset($_SESSION['user'])): ?>
+        <li><a id="startMPGame" href="<?=base_url('index.php/quiz/multiplayer')?>">Грати з іншими</a></li>
+        <li><a href="<?=base_url('index.php/user')?>">Мій профіль</a></li>
+        <?php else: ?>
+        <li><a href="<?=base_url('index.php/login')?>">Увійти/зареєструватися</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
 <main>
