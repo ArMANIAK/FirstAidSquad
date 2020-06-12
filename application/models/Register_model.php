ab483f1b58204e97fa925a359a3bc45a9d0da4c3
ArMANIAK <reklama.alatyr@gmail.com>
2020-06-12 14:44:15 +0300

    Register_model added

---
+++ application/models/Register_model.php
@@ -0,0 +1,22 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Register_model extends CI_Model {
+
+    public function __construct() 
+    {
+        $this->load->database();
+    }
+
+    public function registerCheck($userName) 
+    {
+        $query = $this->db->get_where('users', array('user_login' => $userName));
+        if (count($query->result_array()) !== 0) return false;
+        else return true;
+    }
+
+    public function register($user)
+    {
+        $this->db->insert('users', $user);
+    }
+}
