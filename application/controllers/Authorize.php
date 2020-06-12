85116bd9a5a4f23d4e410ccc94b7b3fa3e597b50
ArMANIAK <reklama.alatyr@gmail.com>
2020-06-12 14:37:03 +0300

    Authorize controller with register method added

---
+++ application/controllers/Authorize.php
@@ -0,0 +1,70 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Authorize extends CI_Controller 
+{
+    private $user_login;
+    private $user_pass_hash;
+
+
+        public function __construct() 
+        {
+			parent::__construct();
+			$this->load->helper('url');
+		}
+		
+        public function index() 
+        {
+			
+			$data['title'] = 'Увійти/зареєструватися';
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('templates/nav');
+			$this->load->view('pages/login');
+			$this->load->view('templates/footer');
+        }
+        
+        public function register() 
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $this->load->model('register_model');
+                if ($this->register_model->registerCheck($_POST['login'])) 
+                {
+                    $user = array(
+                        'user_login' => $_POST['login'],
+                        'user_pass_hash' => md5($_POST['pass'])
+                    );
+                    $this->register_model->register($user);
+
+                    // Log in and redirect to profile
+
+                    // Modal?
+                    echo 'Registered!';
+                    var_dump($_POST);
+                }
+                else
+                {
+                    $data['title'] = 'Увійти/зареєструватися';
+                    $data['message'] = 'Це ім\'я вже зайняте';
+
+                    // print message
+                    $this->load->view('templates/header', $data);
+                    $this->load->view('templates/banner');
+                    $this->load->view('templates/nav');
+                    $this->load->view('templates/error_message', $data);
+                    $this->load->view('pages/login');
+                    $this->load->view('templates/footer');
+                }
+            }
+            else {
+                $data['title'] = 'зареєструватися';
+                
+                $this->load->view('templates/header', $data);
+                var_dump($_POST);
+                
+                die;
+                $this->load->view('templates/footer');
+            }
+            
+        }
+}
